9c47761c4ae221d063b791fcda427088e81759b2
ClyingDeng <1174915260@qq.com>
2020-11-29 20:03:49 +0800

    部署文件

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,16 +2,16 @@
 module.exports = {
     title: 'dying-ui文档', // 设置网站标题
     description: 'dying-ui组件库', // 描述
-    base: './dy-ui-doc/',
-    dest: './dist',
+    base: '/dy-ui-doc/',
+    dest: 'dist',
     // plugins: [
     //     ['@vuepress/search', {
     //         searchMaxSuggestions: 10
     //     }]
     // ],
-    port: 8888, // 端口
+    // port: 8888, // 端口
     themeConfig: {   // 主题配置
-        repo: 'vuejs/vuepress',
+        // repo: 'vuejs/vuepress',
         docsDir: 'docs',
         extend: '@vuepress/theme-default',
         search: false,
@@ -27,7 +27,7 @@ module.exports = {
                 text: '组件',
                 link: '/zh/components/',
             },
-            // { text: 'GitHub', link: 'https://github.com/ClyingDeng/Dy-UI' },
+            { text: 'GitHub', link: 'https://github.com/ClyingDeng/Dy-UI' },
         ],
         // 为以下路由添加侧边栏
         sidebar: {
